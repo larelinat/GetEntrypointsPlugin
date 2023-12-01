f4da168d8468dd9bc630d25ab1f3e8e248a00a0a
Pavel Moskalev <arneshon@gmail.com>
2023-12-01 15:27:56 +1000

    Add jsdock and describe an interfaces.ts

--- GetEntrypointsPlugin.ts
+++ GetEntrypointsPlugin.ts
@@ -37,5 +37,4 @@ export default class GetEntrypointsPlugin implements IGetEntrypointsPlugin {
 
         })
     }
-
 }
--- interfaces.ts
+++ interfaces.ts
@@ -1,14 +1,48 @@
 import {Compiler} from "webpack";
 
+/**
+* Interface for GetEntrypointsPlugin that describe an options.
+*
+* @inteface IGetEntrypointsOptions
+ */
 export interface IGetEntrypointsOptions {
+    /**
+     * File name to save the entrypoints.
+     */
     fileName: string;
 }
 
+/**
+ * Interface for GetEntrypointsPlugin that describe a plugin's class.
+ *
+ * @interface IGetEntrypointsPlugin
+ */
 export interface IGetEntrypointsPlugin {
+
+    /**
+     * Options needed to use the plugin.
+     */
     options: IGetEntrypointsOptions;
+
+    /**
+     * Main function that used to work the plugin.
+     *
+     * @param {Compiler} compiler - The compiler to apply.
+     * @return {void} This function does not return anything.
+     */
     apply(compiler: Compiler): void;
 }
 
+/**
+ * Interface that describes an object used to save the entrypoints.
+ *
+ * @interface IGetEntrypointsEntries
+ * @property {object} entry - The arrays of js and css files.
+ * @property {string[]} entry.js - Array of js files.
+ * @property {string[]} entry.css - Array of css files.
+ * @property {string} publicPath - The public path.
+ * @property {number} version - The version of file.
+ */
 export interface IGetEntrypointsEntries {
     entry: {
         js: string[];
